802a690c478d593ba5adccfa0633e5f10768c692
ewhitebloom <ewhitebloom@gmail.com>
2014-02-28 11:10:31 -0500

    Guessing Game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-alert('Hey find me to know where to put the javascript');
+var random = Math.floor((Math.random()*100)+1);
+
+var name = prompt("What's your name?");
+
+var guess = prompt("Hi " + name + ", guess a number.");
+
+if(guess == random){
+  alert("You won 10 billion dollars!!!!")
+}
+else {
+  alert('Sorry you are a loser .');
+}
+
+
